e7f9c8136c0f63bf4d8fc0e3504998bacdfd2e68
Jon Swenson <jswenson74@gmail.com>
2014-12-09 16:31:47 +0800

    Step-0 Bootstrap
    Basic Angularjs application working with expressjs.

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('my app', function() {
+
+  beforeEach(function() {
+    browser.get('public/index.html');
+  });
+
+});
+
+
